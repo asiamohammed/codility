a73a378911b786eaa2ef2bebbd7bfd34d6cac584
asiamohammed <mohamedasia021@gmail.com>
2023-02-23 16:04:29 +0300

    commit

---
+++ num.rb
@@ -0,0 +1,20 @@
+
+
+
+puts "choose 1st number"
+a = gets
+
+puts "choose 2nd number"
+b = gets
+
+
+case 
+when a < b
+   puts "a is smaller than b"
+when a > b
+   puts  "a is greater than b"
+when a = b
+    puts "a is equal to b"
+
+end
+
